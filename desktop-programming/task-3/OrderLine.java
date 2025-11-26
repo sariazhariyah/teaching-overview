ca7e929ee93f2ee867c1800938326d5afe6ff708
Sari Azhariyah <89737072+sariazhariyah@users.noreply.github.com>
2025-11-26 22:51:29 +0700

    Delete OrderLine class
    Removed the OrderLine class and its associated methods.

--- desktop-programming/task-3/OrderLine.java
+++ desktop-programming/task-3/OrderLine.java
@@ -1,18 +1,1 @@
-/** Satu baris pesanan: item + kuantitas */
-public class OrderLine {
-    private MenuItem item;
-    private int qty;
 
-    public OrderLine(MenuItem item, int qty) {
-        this.item = item;
-        this.qty = qty;
-    }
-
-    public MenuItem getItem() { return item; }
-    public int getQty() { return qty; }
-    public void setQty(int qty) { this.qty = qty; }
-
-    public double getSubTotal() {
-        return item.getHarga() * qty;
-    }
-}
