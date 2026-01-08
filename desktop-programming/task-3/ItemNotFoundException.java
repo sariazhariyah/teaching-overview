9f1428f1be83fa66af9c5694e6fabbdc25629b67
Sari Azhariyah <89737072+sariazhariyah@users.noreply.github.com>
2026-01-08 13:40:47 +0700

    Delete desktop-programming/task-3 directory

--- desktop-programming/task-3/ItemNotFoundException.java
+++
@@ -1,6 +0,0 @@
-// Contoh custom exception untuk memenuhi kriteria Exception
-public class ItemNotFoundException extends Exception {
-    public ItemNotFoundException(String message) {
-        super(message);
-    }
-}
